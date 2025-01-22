cf8d19c08d2b62b7e211d76b0397c084fde7fcd2
Jess Moore <jessclaremoore@gmail.com>
2025-01-22 22:06:36 +1100

    created script to make scripts executable and add to ~/bin

---
+++ add_script.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Time-stamp: <Wednesday 2025-01-22 21:40:10 Jess Moore>
+#
+# Make script executable and add to ${HOME}/bin
+#
+# Usage:
+# bash add_script.sh [script_name]
+
+SCRIPT_FILE=${1}.sh
+COMMAND=${1}
+SCRIPT_DIR="${HOME}/Documents/scripts"
+BIN_DIR="${HOME}/bin"
+
+echo "Script file: ${SCRIPT_DIR}/${SCRIPT_FILE}"
+echo "Command to be created ${COMMAND}"
+
+chmod u+x ${SCRIPT_DIR}/${SCRIPT_FILE}
+ln -s ${SCRIPT_DIR}/${SCRIPT_FILE} ${BIN_DIR}/${COMMAND}
+echo "Created ${COMMAND}"
+ls -l ${HOME}/bin/${COMMAND}
