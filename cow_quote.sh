9f604146b4c687868586c9075430b5e371442d57
Jess Moore <jessclaremoore@gmail.com>
2025-02-25 22:41:17 +1100

    fixed random selection of quote

--- cow_quote.sh
+++ cow_quote.sh
@@ -26,17 +26,23 @@ if [[ -e ${QUOTE_FILE} ]]; then
     # Random quote from fortune and my quotes.json
     # Get quote from fortune
     QUOTE_FORT="$(fortune)"
-    # Get random quote concatenated with author from
-    # user's quotes.json
+
+    # Random quote and author from user's quotes.json
     QUOTE_MY="$(cat $QUOTE_FILE  | jq -c '.[] | [.quote, .author]' | shuf -n 1 | sed 's/[][]//g')"
 
-    echo "$QUOTE_FORT\n$QUOTE_MY" | shuf -n 1 | $(ls ${COWS_BIN}/cow* | shuf -n 1) -f $(ls ${COWS_DIR} | shuf -n 1)
-    echo "(Source: fortune and my ~/quotes.json)."
+    # Randomly select one of the 2 quotes:
+    declare -a QUOTES=("${QUOTE_FORT}" "${QUOTE_MY}")
+    QUOTE_SEL="${QUOTES[$RANDOM % 2]}"
+
+    # Output quote as cowsay/cowthink using all "cow" animals
+    echo "${QUOTE_SEL}" | $(ls ${COWS_BIN}/cow* | shuf -n 1) -f $(ls ${COWS_DIR} | shuf -n 1)
+
+    echo -e "\n(Source: fortune and my ~/quotes.json)."
 
 else
 
     # Old method: Random quote from fortune sources only
     fortune | $(ls /usr/games/cow* | shuf -n 1) -f $(ls /usr/share/cowsay/cows/ | shuf -n 1)
-    echo "(Source: fortune quotes)."
+    echo -e "\n(Source: fortune quotes)."
 
 fi
