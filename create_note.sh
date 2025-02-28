90c3528a559ec732dda6564bc579e44e6012c447
Jess Moore <jessclaremoore@gmail.com>
2025-02-28 15:59:33 +1100

    creates blog note type post

---
+++ create_note.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# Time-stamp: <Friday 2025-02-28 15:55:02 Jess Moore>
+#
+# Create a jekyll website blog note markdown file
+# with some template text
+#
+# Usage: bash create_note.sh install-openhab "How to install OpenHAB"
+
+NAME=`id -F`
+
+# Datetime of now in jekyll blog post format
+NOW=`date "+%Y-%m-%d %H:%M:%S %z"`
+
+# Abbreviated title
+FILEPARTNAME=$1
+# Blog post title
+TITLE=$2
+# Date for post filename
+NOW_DATE_ONLY=`date "+%Y-%m-%d"`
+
+FILENAME=${NOW_DATE_ONLY}-${FILEPARTNAME}.md
+
+cat > ${FILENAME} << EOF
+---
+layout: post
+title:  "${TITLE}"
+date:   ${NOW}
+published: false
+toc: true
+---
+
+Short 1-2 sentence introduction.
+
+**Summary**
+
+1. [command] - 1 line explanation
+2. [command] - 1 line explanation
+
+## Procedure
+
+### Sub heading 1
+
+Text.
+
+See [other post]({% post_url 2025-02-14-other-post %}) for how to do xyz.
+
+[command block]
+
+
+### Sub heading 2
+
+Text.
+
+[command block]
+
+**References**
+
+- [url1]
+- [url2]
+EOF
+
+echo "Successfully created note type post ${FILENAME} with template text."
