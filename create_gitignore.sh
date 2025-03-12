684f98a2e6695c31db3d8bf7a1d3c6aa296db184
Jess Moore <jessclaremoore@gmail.com>
2025-03-12 20:43:45 +1100

    added create_gitignore

---
+++ create_gitignore.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#
+# Time-stamp: <Friday 2025-03-12 20:29:02 Jess Moore>
+#
+# Create a gitignore file
+#
+# Usage: bash create_gitignore.sh
+
+case "${$1}" in
+    flutter)
+
+        echo "Creating .gitignore for: ${TYPE} package type"
+
+    *)
+        echo "Supported options: flutter";
+        exit;;
+esac;
+
+
+# Package type
+TYPE=$1
+
+########################################################################
+if [ "${TYPE}" = "flutter" ]; then
+########################################################################
+
+
+cat > .gitignore << EOF
+# Privacy & Security
+*.env
+
+# Miscellaneous
+*.class
+*.log
+*.pyc
+*.swp
+.DS_Store
+.atom/
+.build/
+.buildlog/
+.history
+.svn/
+.swiftpm/
+migrate_working_dir/
+
+# IntelliJ related
+*.iml
+*.ipr
+*.iws
+.idea/
+
+# The .vscode folder contains launch configuration and tasks you configure in
+# VS Code which you may wish to be included in version control, so this line
+# is commented out by default.
+#.vscode/
+
+# Flutter/Dart/Pub related
+**/doc/api/
+**/ios/Flutter/.last_build_id
+.dart_tool/
+.flutter-plugins
+.flutter-plugins-dependencies
+.pub-cache/
+.pub/
+/build/
+
+# Symbolication related
+app.*.symbols
+
+# Obfuscation related
+app.*.map.json
+
+# Androidss Studio will place build artifacts here
+/android/app/debug
+/android/app/profile
+/android/app/release
+EOF
+########################################################################
+fi
+
+echo "Successfully created .gitignore file for ${TYPE}."
