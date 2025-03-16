89e491b9b79de0a39ee7ce6b36b0b437d28827d8
Jess Moore <jessclaremoore@gmail.com>
2025-03-16 12:09:45 +1100

    Added open_sim command with ios and android options

---
+++ open_sim.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Time-stamp: <Friday 2025-03-16 11:41:12 Jess Moore>
+#
+# Opening mobile development simulator.
+#
+# Usage: bash create_gitignore.sh
+
+# Package type
+case "${1}" in
+    ios|android)
+
+        echo "Opening ${1} simulator.";;
+
+    *)
+        echo "Supported options: ios, android.";
+        exit;;
+esac;
+
+TYPE=$1
+
+########################################################################
+if [ "${TYPE}" = "ios" ]; then
+########################################################################
+open -a Simulator
+########################################################################
+fi
+
+########################################################################
+if [ "${TYPE}" = "android" ]; then
+########################################################################
+~/Library/Android/sdk/emulator/emulator -avd Pixel_3a_Simulator &
+########################################################################
+fi
