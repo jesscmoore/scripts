37cad69d7012bf81974938d0899911d182d8116c
Jess Moore <jessclaremoore@gmail.com>
2025-07-22 20:48:10 +1000

    drafted

--- create_makefile.sh
+++ create_makefile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# Time-stamp: <Friday 2023-04-07 19:44:36 Jess Moore>
+# Time-stamp: <Friday 2025-07-22 15:12:32 Jess Moore>
 #
 # Create a makefile
-# 
-# Usage: create_makefile.sh Makefile
+#
+# Usage: create_makefile.sh
 
 NAME=`id -F`
 NOW=`date "+%A %Y-%m-%d %H:%M:%S"`
@@ -13,4 +13,23 @@ FILENAME=$1
 cat > ${FILENAME} << EOF
 # Time-stamp: <${NOW} ${NAME}>
 #
+SHELL=/bin/bash
+
+########################################################################
+# HELP
+#
+# Help for targets defined in this Makefile.
+
+define HELP
+
+	targetA     1 line description of targetA
+
+endef
+export HELP
+
+help::
+	@echo "$$HELP"
+
+########################################################################
+# Add make target rules here
 EOF
