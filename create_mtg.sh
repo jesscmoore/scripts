80304965f7516f4870845b2c05819146bcc31e72
Jess Moore <jessclaremoore@gmail.com>
2025-07-24 15:37:31 +1000

    automatically generate filename

--- create_mtg.sh
+++ create_mtg.sh
@@ -6,19 +6,47 @@
 #
 # Usage: bash create_mtg.sh 20230407-notes.md "Mtg with xyz" "Jane, Bob, Liz, Sam" "ANU"
 
-NAME=$(id -F)
+function usage() {
+    echo "Usage: $(basename "$0") 'title' 'attendee1, attendee2' 'place'"
+    echo ""
+    echo "Description: This script creates a markdown file for"
+    echo "             meeting notes prepopulated with fields: title, "
+    echo "             date, author, location, and headings for actions, "
+    echo "             talking points, and discussion."
+    echo ""
+    echo "Arguments:"
+    echo "  title:     Meeting title in quotes."
+    echo "  attendees: Comma separated attendees surrounded "
+    echo "             in quotes."
+    echo "  place:     Meeting location in quotes, e.g. Teams."
+    echo ""
+    exit 1 # Exit with a non-zero status to indicate an error
+}
+
+if [[ $# -eq 0 ]]; then
+    usage
+fi
+
+AUTHOR=$(id -F)
 NOW=$(date "+%A %Y-%m-%d %H:%M:%S")
-FILENAME=$1
-TITLE=$2
-ATTENDEES=$3
-PLACE=$4
+TITLE=$1
+ATTENDEES=$2
+PLACE=$3
+
+## Form filename with syntax YYmmdd-person1_person2_author.md
+# Convert attendees to lc, replace ", " with "_"
+PEOPLE_SUMM=$(echo "$ATTENDEES" | awk '{print tolower($0);}' | sed 's/,/ /g;s/  / /g;s/ /_/g')
+# Get lowercase firstname of author
+AUTHOR_SUMM=$(echo "$AUTHOR" | awk '{print tolower($1);}')
+DATE_SUMM=$(date "+%Y%m%d")
+FILENAME="${DATE_SUMM}-${PEOPLE_SUMM}_${AUTHOR_SUMM}.md"
 
 cat > "${FILENAME}" << EOF
 # ${TITLE}
 
-*Date: ${NOW} ${NAME}*
+*Date: ${NOW} ${AUTHOR}*
 
-*Attendees: ${ATTENDEES}*
+*Attendees: ${ATTENDEES}, ${AUTHOR}*
 
 *Location: ${PLACE}*
 
@@ -34,7 +62,7 @@ cat > "${FILENAME}" << EOF
 3.
 4. Next actions/mtg
 
-## Discussions
+## Discussion
 
 
 
