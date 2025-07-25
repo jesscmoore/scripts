6da9bcdf7873a80a1d010f9462878636ba367833
Jess Moore <jessclaremoore@gmail.com>
2025-07-26 07:01:36 +1000

    added usage

--- create_bash.sh
+++ create_bash.sh
@@ -6,17 +6,40 @@
 #
 # Usage: create_bash.sh new_script.sh "Does xyz"
 
-NAME=`id -F`
-NOW=`date "+%A %Y-%m-%d %H:%M:%S"`
-FILENAME=$1
+function usage() {
+    echo "Usage: $(basename "$0") 'file' 'desc'"
+    echo ""
+    echo "Description: This script creates a bash script "
+    echo "             with name [file].sh and standard format "
+    echo "             including datetime author attribution "
+    echo "             stamp, usage line, the user provided"
+    echo "             one line description of what the script"
+    echo "             does. "
+    echo ""
+    echo "Arguments:"
+    echo "  file:      Name of script (excluding .sh)."
+    echo "  desc:      One line description of script."
+    echo ""
+    exit 1 # Exit with a non-zero status to indicate an error
+}
+
+if [[ $# -eq 0 ]]; then
+    usage
+fi
+
+NAME=$(id -F)
+NOW=$(date "+%A %Y-%m-%d %H:%M:%S")
+FILEBASE=$1
 DESC=$2
 
-cat > ${FILENAME} << EOF
+FILENAME=$FILEBASE.sh
+
+cat > "${FILENAME}" << EOF
 #!/bin/bash
 #
 # Time-stamp: <${NOW} ${NAME}>
 #
 # ${DESC}
 #
-# Usage: [add example]
+# Usage: ${FILENAME} [args...]
 EOF
