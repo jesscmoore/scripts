a4377a90dafe985f4122d3a204ff601500384525
Jess Moore <jessclaremoore@gmail.com>
2025-08-04 20:13:41 +1000

    added usage and print results

--- fix_file_names.sh
+++ fix_file_names.sh
@@ -6,16 +6,35 @@
 #
 # - replaces " " with "_"
 # - replaces " - " with "-"
-#
-# Usage:
-#
-# $ fix_file_names
-# or
-# $ fix_file_names "My Spaced File.docx"
+
+function usage() {
+    echo "Usage: $0 [file]"
+    echo ""
+    echo "Description: Converts Windows style filenames with spaces to "
+    echo "linux style whitespace free filenames, either for all files in "
+    echo "the current directory or a specified file."
+    echo ""
+    echo "Arguments:"
+    echo "  file: Name of file to fix the filename of. (No default)."
+    echo ""
+    exit 1 # Exit with a non-zero status to indicate an error
+}
+
+if [[ $* == *"help"* || $* == *"-h"* ]]; then
+    usage
+fi
+
 
 # Filename supplied
 if [[ -n "$1"  &&  -e "$1" ]]; then
     f="$1"
+
+    # Get file type
+    EXT="${f##*.}"
+
+    # Get file extension
+    BASENAME=${f%.*}
+    PREF=${BASENAME:0:3}
 
     if [[ -n $f ]]; then
         # Replace " " with "_"
@@ -35,6 +54,10 @@ if [[ -n "$1"  &&  -e "$1" ]]; then
         else
             echo "No hyphens in spaces found in filename."
         fi
+
+        # Print recently changed matching files
+        # shellcheck disable=SC2012
+        ls -lt ./"$PREF*.$EXT" | head -n 5
     fi
 
 else
@@ -54,5 +77,8 @@ else
             mv "$g" "$i"
         fi
     done
+
+    # Print filenames with most recent first
+    ls -lt
 
 fi
