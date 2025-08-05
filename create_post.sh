196e9fdc161d8991ae36deb9dc31958e1a465b78
Jess Moore <jessclaremoore@gmail.com>
2025-08-05 13:12:47 +1000

    added usage

--- create_post.sh
+++ create_post.sh
@@ -7,21 +7,41 @@
 #
 # Usage: bash create_post.sh install-openhab "How to install OpenHAB"
 
-NAME=`id -F`
+function usage() {
+    echo "Usage: $(basename "$0") 'title' 'attendee1, attendee2' 'place'"
+    echo ""
+    echo "Description: This script creates a Jekyl post file"
+    echo "             in markdown format with fields: filepart "
+    echo "             and title. Posts are not published until "
+    echo "             post YAML is changed to 'published: true'"
+    echo "             and content built and pushed to web server."
+    echo ""
+    echo "Arguments:"
+    echo "  title:     Blog post title."
+    echo "  filepart:  The text component of the filename. "
+    echo "             Used to construct filename with format"
+    echo "             YYYYMMDD-file-part.md."
+    echo ""
+    exit 1 # Exit with a non-zero status to indicate an error
+}
+
+if [[ $# -eq 0 || $* == *"help"* || $* == *"-h"* ]]; then
+    usage
+fi
 
 # Datetime of now in jekyll blog post format
-NOW=`date "+%Y-%m-%d %H:%M:%S %z"`
+NOW=$(date "+%Y-%m-%d %H:%M:%S %z")
 
 # Abbreviated title
 FILEPARTNAME=$1
 # Blog post title
 TITLE=$2
 # Date for post filename
-NOW_DATE_ONLY=`date "+%Y-%m-%d"`
+NOW_DATE_ONLY=$(date "+%Y-%m-%d")
 
 FILENAME=${NOW_DATE_ONLY}-${FILEPARTNAME}.md
 
-cat > ${FILENAME} << EOF
+cat > "${FILENAME}" << EOF
 ---
 layout: post
 title:  "${TITLE}"
