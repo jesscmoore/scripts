a4f5fa890f9485a259361adbb8f2fb6e7bbdf3f7
Jess Moore <jessclaremoore@gmail.com>
2025-08-18 13:45:05 +1000

    creates markdown file for drafting an email

---
+++ draft_email.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# Time-stamp: <Monday 2025-08-18 13:05:58 Jess Moore>
+#
+# Create a draft of an email in markdown
+#
+# Usage: draft_email.sh [args...]
+
+function usage() {
+    echo "Usage: draft_email.sh [args...]"
+    echo ""
+    echo "Description: Create a draft of an email in markdown."
+    echo ""
+    echo "Arguments:"
+    echo "  arg1:      ..."
+    echo "  arg2:      ..."
+    echo ""
+    exit 1 # Exit with a non-zero status to indicate an error
+}
+
+if [[ $# -eq 0 || $* == *"help"* || $* == *"-h"* ]]; then
+    usage
+fi
+
+RECIPIENT=$1
+SENDER=$(id -F)
+NOW=$(date "+%A %Y-%m-%d %H:%M:%S")
+
+## Form filename with syntax YYmmdd-draftEmailTo-person.md
+# Convert recipient to lower Camel Case
+PEOPLE_SUMM=$(echo "$RECIPIENT" | awk '{print tolower($0);}' | awk '{print $1, toupper(substr($2,1,1)) substr($2,2)}' | sed 's/ //g')
+
+SENDER_FIRSTNAME=$(echo "$SENDER" | awk '{print $1}')
+
+DATE_SUMM=$(date "+%Y%m%d")
+# Form filename
+FILENAME="${DATE_SUMM}-draftEmailTo-${PEOPLE_SUMM}.md"
+
+
+cat > "${FILENAME}" << EOF
+*From: ${SENDER}*
+
+*To: ${RECIPIENT}*
+
+**Subject: [ADD]**
+
+*Date: ${NOW} ${SENDER}*
+
+Hi [ADD]
+
+
+
+
+
+Regards,
+${SENDER_FIRSTNAME}
+
+[ADD SIGNATURE]
+
+
+
+
+
+<!-- markdownlint-disable-file MD029 MD036 MD041 -->
+EOF
+
+echo "Done"
+echo "Output: "
+ls -l "$FILENAME"
