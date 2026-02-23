94ce08d17e93f6a06f85dfb576a3319ae4265e88
Jess Moore <jessclaremoore@gmail.com>
2026-02-23 22:36:41 +1100

    create copy of tagged file

--- create_git_tag.sh
+++ create_git_tag.sh
@@ -7,31 +7,45 @@
 # Usage: create_git_tag.sh [args...]
 
 function usage() {
-    echo "Usage: create_git_tag.sh [tag_num] [comment]"
+    echo "Usage: create_git_tag.sh [tag_num] [comment] [file]"
     echo ""
     echo "Description: Write a git tag of a file."
     echo ""
     echo "Arguments:"
     echo "  tag_num:    Tag version number. (Default: none)."
     echo "  comment:    Tag comment. (Default: none)."
+    echo "  file:       Optional filename to append tag to and copy "
+    echo "              to shared_and_tagged/ folder"
     echo ""
     exit 1 # Exit with a non-zero status to indicate an error
 }
 
-if [[ $# -ne 2 || $* == *"help"* || $* == *"-h"* ]]; then
+if [[ $# -lt 2 || $# -gt 3  || $* == *"help"* || $* == *"-h"* ]]; then
     usage
 fi
 
 TAG=$1
 COMMENT=$2
+if [[ $# -eq 3 ]]; then 
+    FILE=$3
+fi
 
 git tag -a "v${TAG}" -m "${COMMENT}"
 
 echo "Pushing tags"
-git push origin --tags
+git push --tags
 
 echo ""
 echo "More info:"
 echo "git show v${TAG}"
 echo "git log --pretty=oneline --abbrev-commit"
+
+if [[ $# -eq 3 ]]; then 
+    EXT="${FILE##*.}"
+    BASENAME="${FILE%.*}"
+    mkdir -p shared_and_tagged
+    cp -p "${FILE}" "shared_and_tagged/${BASENAME}-v${TAG}.${EXT}"
+    ls -lt shared_and_tagged
+fi
+
 echo "Done."
