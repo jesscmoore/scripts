0d0a2e0fe37c815aac443f6d8cb395af0abb25df
Jess Moore <jessclaremoore@gmail.com>
2026-04-26 15:42:38 +1000

    added support for other remote locations

--- my_pull.sh
+++ my_pull.sh
@@ -1,44 +1,62 @@
 #!/bin/bash
 #
-# Time-stamp: Sunday 2026-04-12 09:52:33 Jess Moore
+# Time-stamp: Sunday 2026-04-26 Jess Moore
 #
-# Push local document to remote Nextcloud folder
+# Pull local document from a remote sync folder
 #
-# Usage: my_pull.sh [args...]
+# Usage: my_pull.sh [-n|-p|-o folder] [filename] [remotesubdir]
 
 function usage() {
-    echo "Usage: my_pull.sh [filename] [remotesubdir]"
+    echo "Usage: my_pull.sh [-n|-p|-o folder] [filename] [remotesubdir]"
     echo ""
-    echo "Description: Pull local document from remote Nextcloud folder."
-    echo "This script pulls document from local sync drive of remote Nextcloud"
-    echo "folder. Check Nextcloud online afterwards to confirm syncing is up to"
-    echo "date."
+    echo "Description: Pull a document from a remote sync folder."
+    echo "This script copies a document from a subdirectory of the chosen"
+    echo "remote folder into the current directory. Check the remote"
+    echo "afterwards to confirm syncing is up to date."
+    echo ""
+    echo "Flags (exactly one required):"
+    echo "  -n             Nextcloud folder  (~/Documents/Nextcloud)"
+    echo "  -p             Private folder    (~/Documents/private)"
+    echo "  -o folder      Other folder      (~/Documents/<folder>)"
     echo ""
     echo "Arguments:"
     echo "  filename:      Name of file (must include extension)."
-    echo "  remotesubdir:  Sub directory on local sync dir to remote location"
+    echo "  remotesubdir:  Subdirectory within the remote folder."
     echo ""
-    exit 1 # Exit with a non-zero status to indicate an error
+    exit 1
 }
 
+BASE_DIR=""
 
-if [[ $# -ne 2 || $* == *"help"* || $* == *"-h"* ]]; then
+[[ "$1" == "--help" ]] && usage
+
+while getopts ":hnpo:" opt; do
+    case $opt in
+        h) usage ;;
+        n) BASE_DIR="${HOME}/Documents/Nextcloud" ;;
+        p) BASE_DIR="${HOME}/Documents/private" ;;
+        o) BASE_DIR="${HOME}/Documents/${OPTARG}" ;;
+        *) usage ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+if [[ -z "$BASE_DIR" || $# -ne 2 ]]; then
     usage
 fi
 
 FILE=$1
 REM_SUB_DIR=$2
 
-
 # User
 user=$(whoami)
-REM_DIR="${HOME}/Documents/Nextcloud/${REM_SUB_DIR}"
+REM_DIR="${BASE_DIR}/${REM_SUB_DIR}"
 
 # Check remote dir exists
-if [[ ! -d ${REM_DIR} ]]; then 
+if [[ ! -d ${REM_DIR} ]]; then
     echo "Remote directory does not exist"
     exit 1
-else 
+else
     echo "Remote directory: ${REM_DIR}"
 fi
 
@@ -48,32 +66,32 @@ if [[ ! -e "${REM_DIR}/$FILE" ]]; then
   exit 1
 fi
 
-if [[ "${user}" == "u9904893" ]]; then 
+if [[ "${user}" == "u9904893" ]]; then
 
-    if [[ -f "${FILE}" ]]; then 
+    if [[ -f "${FILE}" ]]; then
         echo "Backing up local file to *.bak"
         cp -p "${FILE}" "${FILE}.bak"
     fi
-    
-     echo "Files on Nextcloud:"
+
+    echo "Files on remote:"
     FILES_REM=$(find "${REM_DIR}" -name "${FILE}*")
     for f in $FILES_REM
     do
         ls -lt "$f"
     done
-    
+
     while true; do
-        read -r -p "Do you wish to pull ${FILE} from Nextcloud?: " yn
+        read -r -p "Do you wish to pull ${FILE} from remote?: " yn
         case $yn in
             [Yy]* ) echo "Proceeding..."; break;;
             [Nn]* ) echo "Aborting..."; exit;;
             * ) echo "Please answer y or n.";;
         esac
-    done    
-    
-    echo "Pulling from ${REM_DIR}..."    
+    done
+
+    echo "Pulling from ${REM_DIR}..."
     cp -p "${REM_DIR}/${FILE}" "${FILE}"
-    
+
 fi
 
 echo "Done"
--- my_push.sh
+++ my_push.sh
@@ -1,44 +1,62 @@
 #!/bin/bash
 #
-# Time-stamp: Wednesday 2026-03-18 14:28:16 Jess Moore
+# Time-stamp: Sunday 2026-04-26 Jess Moore
 #
-# Push local document to remote Nextcloud folder
+# Push local document to a remote sync folder
 #
-# Usage: my_push.sh [args...]
+# Usage: my_push.sh [-n|-p|-o folder] [filename] [remotesubdir]
 
 function usage() {
-    echo "Usage: my_push.sh [filename] [remotesubdir]"
+    echo "Usage: my_push.sh [-n|-p|-o folder] [filename] [remotesubdir]"
     echo ""
-    echo "Description: Push local document to remote Nextcloud folder."
-    echo "This script pushs a local document to the local sync drive of"
-    echo "remote Nextcloud folder, ie. the subdirectory of Nextcloud folder."
-    echo "Check Nextcloud online afterwards to confirm syncing is up to date."
+    echo "Description: Push local document to a remote sync folder."
+    echo "This script copies a local document into a subdirectory of the"
+    echo "chosen remote folder. Check the remote afterwards to confirm"
+    echo "syncing is up to date."
+    echo ""
+    echo "Flags (exactly one required):"
+    echo "  -n             Nextcloud folder  (~/Documents/Nextcloud)"
+    echo "  -p             Private folder    (~/Documents/private)"
+    echo "  -o folder      Other folder      (~/Documents/<folder>)"
     echo ""
     echo "Arguments:"
     echo "  filename:      Name of file (must include extension)."
-    echo "  remotesubdir:  Sub directory on local sync dir to remote location"
+    echo "  remotesubdir:  Subdirectory within the remote folder."
     echo ""
-    exit 1 # Exit with a non-zero status to indicate an error
+    exit 1
 }
 
+BASE_DIR=""
 
-if [[ $# -ne 2 || $* == *"help"* || $* == *"-h"* ]]; then
+[[ "$1" == "--help" ]] && usage
+
+while getopts ":hnpo:" opt; do
+    case $opt in
+        h) usage ;;
+        n) BASE_DIR="${HOME}/Documents/Nextcloud" ;;
+        p) BASE_DIR="${HOME}/Documents/private" ;;
+        o) BASE_DIR="${HOME}/Documents/${OPTARG}" ;;
+        *) usage ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+if [[ -z "$BASE_DIR" || $# -ne 2 ]]; then
     usage
 fi
 
 FILE=$1
 REM_SUB_DIR=$2
 
-
 # User
 user=$(whoami)
-REM_DIR="${HOME}/Documents/Nextcloud/${REM_SUB_DIR}"
+REM_DIR="${BASE_DIR}/${REM_SUB_DIR}"
 
 # Check remote dir exists
-if [[ ! -d ${REM_DIR} ]]; then 
+if [[ ! -d ${REM_DIR} ]]; then
     echo "Remote directory does not exist"
     exit 1
-else 
+else
     echo "Remote directory: ${REM_DIR}"
 fi
 
@@ -48,23 +66,23 @@ if [[ ! -e "$FILE" ]]; then
   exit 1
 fi
 
-if [[ "${user}" == "u9904893" ]]; then 
+if [[ "${user}" == "u9904893" ]]; then
 
-    if [[ -f "${REM_DIR}/${FILE}" ]]; then 
+    if [[ -f "${REM_DIR}/${FILE}" ]]; then
         echo "Backing up remote file to *.bak"
         cp -p "${REM_DIR}/${FILE}" "${REM_DIR}/${FILE}.bak"
     fi
-    
-    echo "Pushing to ${REM_DIR}..."    
+
+    echo "Pushing to ${REM_DIR}..."
     cp -p "$FILE" "${REM_DIR}"/.
-    
-    echo "Files on Nextcloud:"
+
+    echo "Files on remote:"
     FILES_REM=$(find "${REM_DIR}" -name "${FILE}*")
     for f in $FILES_REM
     do
         ls -lt "$f"
     done
-    
+
 fi
 
 echo "Done"
